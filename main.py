1bce725c7f6fd1b6b745639b662ce3f58729dc4d
MatkoCilingac <ceniga16@gmail.com>
2026-05-04 16:25:46 +0200

    adding minor changes

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ sys.path.append('./exercise_9')
 sys.path.append('./microdot_libraries')
 sys.path.append('./temnplates')
 sys.path.append('./webpage')
-
+# git add .
+# git commit -m "popis"
+# git push
 
 
 import webpage.webpage_application
